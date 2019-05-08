2600e0141c7edda64260e2b590d90a49ea240f10
BuBiG <nomand555@gmail.com>
2019-05-08 14:19:02 +0700

    Register Success

---
+++ app/src/main/java/th/ac/rmutsv/meefoodshop/MyConstant.java
@@ -0,0 +1,10 @@
+package th.ac.rmutsv.meefoodshop;
+
+public class MyConstant {
+
+    private String urlAddUser = "https://www.androidthai.in.th/ruts/adduserbig.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
